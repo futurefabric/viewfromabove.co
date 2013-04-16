4da50898b6a5cfbe24a860a620303bbb779d5f7d
Guy Moorhouse <guy@futurefabric.co.uk>
2013-04-16 11:42:31 +0100

    Blacklisting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,13 @@ class HomeController < ApplicationController
   def index
 
   	@body_class = "the_view"
+    twitter_account = "Cmdr_Hadfield"
   	
   	if params[:max_id] != nil
   		tweet_max_id = params[:max_id].to_s
-			@tweets = Twitter.user_timeline("Cmdr_Hadfield", :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id)
+			@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id)
 		else
-			@tweets = Twitter.user_timeline("Cmdr_Hadfield", :exclude_replies => true, :include_rts => false, :count => 100)
+			@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100)
 		end
 
   end
