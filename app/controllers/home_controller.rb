5716cf21c7c7bf01655d543d9c47c2ef90506aa7
Guy Moorhouse <gmoorhouse@gmail.com>
2013-10-22 14:46:04 +0100

    Added blacklisted items to array. Further work on limiting displayed tweets to correct since and max ids

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,14 +7,14 @@ class HomeController < ApplicationController
     twitter_account_nyberg    = "AstroKarenN"
   	
   	if params[:max_id] != nil
+
   		tweet_max_id = params[:max_id].to_s
-			#@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id)
-      @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705")
-      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id)
+      
+      @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705", :since_id => "300000000000000000")
+      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id, :since_id => "339350311237976064")
 		else
-			#@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705")
-		  @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705")
-      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "999999999999999999")
+		  @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705", :since_id => "300000000000000000")
+      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "999999999999999999", :since_id => "339350311237976064")
     end
 
     @tweets = @tweets_nyberg.zip(@tweets_hadfield).flatten.compact
