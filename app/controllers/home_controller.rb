6b190a9e150ef6bff6ab69f70c7d6eef454e3422
Guy Moorhouse <gmoorhouse@gmail.com>
2013-10-22 15:29:53 +0100

    Updated logic to cater for beginning and endpoints of two users feeds. Bit hacky, but works for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,24 +2,49 @@ class HomeController < ApplicationController
 
   def index
 
-  	@body_class = "the_view"
+    @body_class = "the_view"
+
+    # twitter accounts
     twitter_account_hadfield  = "Cmdr_Hadfield"
     twitter_account_nyberg    = "AstroKarenN"
-  	
-  	if params[:max_id] != nil
 
-  		tweet_max_id = params[:max_id].to_s
-      
-      @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705", :since_id => "300000000000000000")
-      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id, :since_id => "339350311237976064")
-		else
-		  @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705", :since_id => "300000000000000000")
-      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "999999999999999999", :since_id => "339350311237976064")
+    # generic start and end IDs
+    twitter_start_id = "300000000000000000"
+    twitter_end_id = "999999999999999999"
+
+    # user account start and end IDs
+    twitter_hadfield_since_id = "300000000000000000"
+    twitter_hadfield_max_id = "334011022815944705"
+
+    twitter_nyberg_since_id = "339350311237976064"
+    twitter_nyberg_max_id = "999999999999999999"
+    
+
+    # a max_id has been passed through the url (from pagination link)
+    if params[:max_id] != nil
+      tweet_max_id = params[:max_id].to_s
+
+      # last of AstroKarenN’s tweets we want
+      if tweet_max_id < "341299897074581506"
+        twitter_hadfield_max_id = tweet_max_id
+      else
+        twitter_nyberg_max_id = tweet_max_id
+      end
+
+    # no max_id
+    else
+
     end
 
+    # get the tweets
+    @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => twitter_hadfield_max_id, :since_id => twitter_hadfield_since_id)
+    @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => twitter_nyberg_max_id, :since_id => twitter_nyberg_since_id)
+
+    # collate users tweets together
     @tweets = @tweets_nyberg.zip(@tweets_hadfield).flatten.compact
+
+    # sort tweets by date created
     @tweets.sort! { |a,b| b.created_at <=> a.created_at }
-    #.attrs[:created_at]
   end
 
 end
