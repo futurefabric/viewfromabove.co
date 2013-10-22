be522310b895716a5b09c86a66770e421ce030f0
Guy Moorhouse <gmoorhouse@gmail.com>
2013-10-22 15:59:51 +0100

    Still trying to nail the logic here. Further blacklisting for some Hadfield photos where the astronauts posting times overlap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,28 +13,29 @@ class HomeController < ApplicationController
     twitter_end_id = "999999999999999999"
 
     # user account start and end IDs
-    twitter_hadfield_since_id = "300000000000000000"
+    twitter_hadfield_since_id = twitter_start_id
     twitter_hadfield_max_id = "334011022815944705"
 
     twitter_nyberg_since_id = "339350311237976064"
-    twitter_nyberg_max_id = "999999999999999999"
+    twitter_nyberg_max_id = twitter_end_id
     
 
     # a max_id has been passed through the url (from pagination link)
     if params[:max_id] != nil
-      tweet_max_id = params[:max_id].to_s
+      tweet_max_id = params[:max_id]
+      twitter_nyberg_max_id = tweet_max_id.to_s
 
-      # last of AstroKarenN’s tweets we want
-      if tweet_max_id < "341299897074581506"
-        twitter_hadfield_max_id = tweet_max_id
-      else
-        twitter_nyberg_max_id = tweet_max_id
+      # the point at which we need to paginate Cmdr_Hadfield's tweets
+      if tweet_max_id < "347467406974783488"
+        twitter_hadfield_max_id = tweet_max_id.to_s
       end
 
     # no max_id
     else
 
     end
+
+
 
     # get the tweets
     @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => twitter_hadfield_max_id, :since_id => twitter_hadfield_since_id)
