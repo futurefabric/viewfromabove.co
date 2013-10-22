97a30c4319a58a4408fe871a4bac111b5ba54570
Guy Moorhouse <gmoorhouse@gmail.com>
2013-10-22 13:32:14 +0100

    Pulling in Karen Nyberg’s photographs. Munged together both hers and Hadfield's together and then sorted by date.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,23 @@ class HomeController < ApplicationController
   def index
 
   	@body_class = "the_view"
-    twitter_account = "Cmdr_Hadfield"
+    twitter_account_hadfield  = "Cmdr_Hadfield"
+    twitter_account_nyberg    = "AstroKarenN"
   	
   	if params[:max_id] != nil
   		tweet_max_id = params[:max_id].to_s
-			@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id)
+			#@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id)
+      @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705")
+      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => tweet_max_id)
 		else
-			@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705")
-		end
+			#@tweets = Twitter.user_timeline(twitter_account, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705")
+		  @tweets_hadfield = Twitter.user_timeline(twitter_account_hadfield, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "334011022815944705")
+      @tweets_nyberg = Twitter.user_timeline(twitter_account_nyberg, :exclude_replies => true, :include_rts => false, :count => 100, :max_id => "999999999999999999")
+    end
 
+    @tweets = @tweets_nyberg.zip(@tweets_hadfield).flatten.compact
+    @tweets.sort! { |a,b| b.created_at <=> a.created_at }
+    #.attrs[:created_at]
   end
 
 end
