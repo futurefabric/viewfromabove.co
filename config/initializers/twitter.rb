81a8c9f4451b46bfddbff94fcd5744881b9846a9
Guy Moorhouse <gmoorhouse@gmail.com>
2013-12-14 23:10:32 +0000

    Set up environment variables to allow for different dev/prod time windows before tweets show. Also moved twitter credentials out of the app

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = "cc3vr835XIYhwgwtlzpa6w"
-  config.consumer_secret = "LXH7sInaO2AGndlrchRtit0eOuNBaLXvUdF9eckQ"
-  config.oauth_token = "5340042-bwePoWsMBIZWF9a2KbLSw7ERYPMVffkE1ofHunk2Wk"
-  config.oauth_token_secret = "b5hlSBpE9hSYRrhXF6geozJ6GzyGFVv0uqcqh4gkS6w"
+  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+  config.oauth_token = ENV["TWITTER_OAUTH_TOKEN"]
+  config.oauth_token_secret = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
 end
